71b78573cba489ec6ea5dc578f690e2755d81961
Sudhanshu Mishra <mrsud94@gmail.com>
2013-11-05 11:37:17 +0530

    After lab9

--- BankProject/ATMManager.java
+++ BankProject/ATMManager.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 /**
  * This class uses BankAccount class to implement ATM management for an account
  * @author mrsud
@@ -65,11 +66,37 @@ public class ATMManager {
 		    System.out.println(entry.getKey() + " " + entry.getValue().personal.name);
 		}
 		
-		
+		Map<String, BankAccount> duplicateAddress = new HashMap<String, BankAccount>();
 		Set<String> addresses = new HashSet<String>();
 		for(int i=0; i<5; i++){
-			if(!addresses.add(accounts[i].personal.address))
-				System.out.println("Duplicate address found");
+			if(!addresses.add(accounts[i].personal.address)){
+				//System.out.println("Duplicate address found");
+				duplicateAddress.put(String.valueOf(accounts[i].accountNo), accounts[i]);
+			}
+		}
+		
+		String tempAddress;
+		Map<String, ArrayList<BankAccount>> duplicateAddressAcc = new HashMap<String, ArrayList<BankAccount>>();
+		for(Map.Entry<String, BankAccount> entry : duplicateAddress.entrySet()){
+			tempAddress = entry.getValue().personal.address;
+			ArrayList<BankAccount> tempAcc = new ArrayList<BankAccount>();
+			for(int i=0;i<5;i++){
+				if(accounts[i].personal.address.intern()==tempAddress.intern()){
+					tempAcc.add(accounts[i]);	
+				}			
+			}
+			duplicateAddressAcc.put(entry.getValue().personal.address, tempAcc);
+
+		}
+		
+
+		
+		System.out.println("\nAccounts having duplicate address:");
+		for (Entry<String, ArrayList<BankAccount>> entry : duplicateAddressAcc.entrySet()){
+		    String tempAdd = entry.getKey();
+		    for(int i=0; i<entry.getValue().size();i++){
+		    	System.out.println(tempAdd + " " + entry.getValue().get(i).accountNo);
+		    }
 		}
 		
 		System.out.println("\nAccounts with more than 100000 balance");
